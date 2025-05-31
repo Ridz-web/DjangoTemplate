eb935a451aebbea3469dad94c8cc7d719aebf05b
Ridz-Web <mutaalibudi@gmail.com>
2025-05-31 19:33:20 +0700

    template dasar django

--- mysite/Home/views.py
+++ mysite/Home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     nama = 'ridho'
-    return render(request, 'index.html', {'nama': nama})
+    return render(request, 'Home/index.html', {'nama': nama})
 
 def blog(request):
-    return render(request, 'blog.html')
+    return render(request, 'Home/blog.html')
